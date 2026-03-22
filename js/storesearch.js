0bffd16e677755f182a41387beab2895c93734e1
rushda-101 <rushda.afaf@gmail.com>
2026-03-22 20:31:34 +0000

    Added Map API function and search bar

--- js/storesearch.js
+++ js/storesearch.js
@@ -12,3 +12,47 @@ const stores = [
     { name: "Luton", lat: 51.8787, lon: -0.4200 },
     { name: "Watford", lat: 51.6565, lon: -0.3903 }
 ];
+
+function getDistance(lat1, lon1, lat2, lon2) {
+    return Math.sqrt(
+        (lat2 - lat1) ** 2 + (lon2 - lon1) ** 2
+    );
+}
+
+function findNearest(userLat, userLon) {
+    let nearest = stores[0];
+    let minDist = Infinity;
+
+    stores.forEach(store => {
+        const dist = getDistance(userLat, userLon, store.lat, store.lon);
+        if (dist < minDist) {
+            minDist = dist;
+            nearest = store;
+        }
+    });
+
+    return nearest;
+}
+
+let map = L.map('map').setView([51.5, -0.1], 10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+async function handleSearch() {
+    const query = document.getElementById("searchInput").value;
+
+    const location = await searchLocation(query);
+
+    const userLat = location.lat;
+    const userLon = location.lon;
+
+    const nearest = findNearest(userLat, userLon);
+
+    map.setView([userLat, userLon], 12);
+
+    L.marker([userLat, userLon]).addTo(map)
+        .bindPopup("You are here");
+
+    L.marker([nearest.lat, nearest.lon]).addTo(map)
+        .bindPopup(nearest.name);
+}
